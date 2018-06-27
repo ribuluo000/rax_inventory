15956d0df3ebdea2eef4c9b2a08af15c41506cac
Nick <ribuluo000@163.com>
2018-06-27 11:45:44 +0800

    1

--- inventory/src/index.js
+++ inventory/src/index.js
@@ -1,6 +1,6 @@
 import {createElement, render} from 'rax';
 // import App from './App';
-// import App from './demo/Demo_Canvas';
+// import App from './demo/demo_canvas';
 // import App from './demo/demo_charts';
 // import App from './demo/demo_drag';
 // import App from './demo/demo_webgl';
