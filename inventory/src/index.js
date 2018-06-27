31a5168e031c1ddbca1ce08aabe49372fc000d69
Nick <ribuluo000@163.com>
2018-06-27 11:43:37 +0800

    demo_component

--- inventory/src/index.js
+++ inventory/src/index.js
@@ -5,6 +5,7 @@ import {createElement, render} from 'rax';
 // import App from './demo/demo_drag';
 // import App from './demo/demo_webgl';
 // import App from './demo/demo_perf';
-import App from './demo/demo_parallax';
+// import App from './demo/demo_parallax';
+import App from './demo/demo_component';
 
 render(<App />);
