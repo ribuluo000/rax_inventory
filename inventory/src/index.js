4fd4b82ae424026fe1194200f567b954a2d512c1
Nick <ribuluo000@163.com>
2018-06-27 11:05:41 +0800

    集成 	universal-perf

--- inventory/src/index.js
+++ inventory/src/index.js
@@ -3,6 +3,7 @@ import {createElement, render} from 'rax';
 // import App from './demo/Demo_Canvas';
 // import App from './demo/demo_charts';
 // import App from './demo/demo_drag';
-import App from './demo/demo_webgl';
+// import App from './demo/demo_webgl';
+import App from './demo/demo_perf';
 
 render(<App />);
