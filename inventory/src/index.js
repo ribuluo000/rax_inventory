5e1f5b53c1a1f5bcd9c7e8c69521bbcfaf2256bb
Nick <ribuluo000@163.com>
2018-06-27 10:56:33 +0800

    集成 	rax-charts

--- inventory/src/index.js
+++ inventory/src/index.js
@@ -1,5 +1,6 @@
 import {createElement, render} from 'rax';
 // import App from './App';
-import App from './demo/Demo_Canvas';
+// import App from './demo/Demo_Canvas';
+import App from './demo/demo_charts';
 
 render(<App />);
