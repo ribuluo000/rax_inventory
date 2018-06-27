08829dc4c30548a9bab6c7fea83c0feb727d639e
Nick <ribuluo000@163.com>
2018-06-27 10:59:19 +0800

    集成 	universal-panresponder

--- inventory/src/index.js
+++ inventory/src/index.js
@@ -1,6 +1,7 @@
 import {createElement, render} from 'rax';
 // import App from './App';
 // import App from './demo/Demo_Canvas';
-import App from './demo/demo_charts';
+// import App from './demo/demo_charts';
+import App from './demo/demo_drag';
 
 render(<App />);
