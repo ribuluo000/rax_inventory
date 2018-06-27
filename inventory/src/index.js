759a8c3d348d0e0350d67de31e231a62e5873514
Nick <ribuluo000@163.com>
2018-06-27 11:03:09 +0800

    集成 	driver-webgl

--- inventory/src/index.js
+++ inventory/src/index.js
@@ -2,6 +2,7 @@ import {createElement, render} from 'rax';
 // import App from './App';
 // import App from './demo/Demo_Canvas';
 // import App from './demo/demo_charts';
-import App from './demo/demo_drag';
+// import App from './demo/demo_drag';
+import App from './demo/demo_webgl';
 
 render(<App />);
