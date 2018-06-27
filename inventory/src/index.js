b8eea5494472eb8e61dbf6b8fc90c0249f378dee
Nick <ribuluo000@163.com>
2018-06-27 15:33:13 +0800

    animated

--- inventory/src/index.js
+++ inventory/src/index.js
@@ -1,5 +1,5 @@
 import {createElement, render} from 'rax';
-// import App from './App'; //production success
+import App from './App'; //production success
 // import App from './demo/demo_canvas';    //production error on android
 // import App from './demo/demo_charts';   //production error on android
 // import App from './demo/demo_drag';  //production error on android
@@ -7,7 +7,8 @@ import {createElement, render} from 'rax';
 // import App from './demo/demo_perf';     //production error
 // import App from './demo/demo_parallax';  //production some error on android
 // import App from './demo/demo_component'; //production error
-import App from './demo/demo_modules';   //production error on android
+// import App from './demo/demo_modules';   //production error on android
 // import App from './demo/demo_redux_todos';   //production some error on android
+// import App from './demo/demo_animated';   //production some error on android
 
 render(<App />);
